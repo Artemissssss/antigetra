9033994d602ad5cc4584b17638d6b7f8baed9968
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-04 20:10:06 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,6 +1,6 @@
 import TeleBot from "telebot"
-import { MongoClient } from 'mongodb';
 import openai from 'openai'
+const { MongoClient } = require('mongodb');
 const openaiClient = new openai(process.env.OPENAI_API_KEY);
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
