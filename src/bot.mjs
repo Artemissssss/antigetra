9f6fd6d5f30679f8a4a548357191aa798aac6690
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-04 14:33:20 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,6 +2,18 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on("text", msg => {
+    const chatId = msg.chat.id;
+    const messageId = msg.message_id;
+    const text = msg.text;
+    let banStatus = false;
+    const banWords = ["#stop_lgbt_propaganda"];
+    for(let i = 0; i<banWords.length;i++){
+        if(text.includes(banWords[0])){
+            banStatus = true;
+        };
+    };
+    banStatus ? bot.deleteMessage(chatId, messageId) : null;
+})
 
 export default bot
