6f08e1f452ba76f2568cd7a5768634d8a6e5e980
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-04 14:13:13 +0200

    Add files via upload

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,8 +2,6 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
-
-bot.start();
+bot.on("text", msg => msg.reply.text(msg.text))
 
 export default bot
