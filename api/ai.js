3479e07c5b017d9eae37074e20c4cde4d83c6f5f
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 00:47:23 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,19 +1,21 @@
 import Bard from "bard-ai";
- 
+
 export default async function handler(req, res) {
- 
-    await Bard.init("sidts-CjEBSAxbGaKMRv-GMYaEE-c8DzVe_W7ZPC9ktwD2hPvenlS02XiYpdkzFEj7bRSEXxbAEAA");
-    let myConversation =new Bard.Chat();
-    try {
-      
-        // Use the askAI function here
-        const response = await myConversation.ask("How are you?")
-        res.status(200).json({ name: response })
-      
-        // Rest of your code
-      } catch (error) {
-        res.status(404).json({error:error})
-        console.error("Error:", error);
-      }
-    // console.log(await );
+  try {
+    // Initialize Bard with your API key
+    Bard.init("sidts-CjEBSAxbGaKMRv-GMYaEE-c8DzVe_W7ZPC9ktwD2hPvenlS02XiYpdkzFEj7bRSEXxbAEAA");
+
+    // Create a new chat instance
+    const myConversation = new Bard.Chat();
+
+    // Use the askAI function here
+    const response = await myConversation.ask("How are you?");
+    
+    // Send the response back as JSON
+    res.status(200).json({ response: response });
+
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
+}
