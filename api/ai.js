e9a4344e3f3c7d1d4db46376f3884576d38f3949
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 00:41:31 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,9 +1,18 @@
-import Bard,{ askAI } from "bard-ai";
+import Bard, { askAI } from "bard-ai";
 
 export default async function handler(req, res) {
  
     await Bard.init("sidts-CjEBSAxbGaKMRv-GMYaEE-c8DzVe_W7ZPC9ktwD2hPvenlS02XiYpdkzFEj7bRSEXxbAEAA");
-     
+    try {
+      
+        // Use the askAI function here
+        const response = await askAI("Your question");
+        res.status(200).json({ name: response })
+      
+        // Rest of your code
+      } catch (error) {
+        res.status(404).json("bl")
+        console.error("Error:", error);
+      }
     // console.log(await );
-    res.status(200).json({ name: await askAI("Hello world!") })
   }
