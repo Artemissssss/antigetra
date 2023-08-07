8b1bd081693070f4dddf0dc54a3b2741521c2252
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 00:44:19 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,12 +1,13 @@
-import Bard, { askAI } from "bard-ai";
-
+import Bard from "bard-ai";
+ 
 export default async function handler(req, res) {
  
     await Bard.init("sidts-CjEBSAxbGaKMRv-GMYaEE-c8DzVe_W7ZPC9ktwD2hPvenlS02XiYpdkzFEj7bRSEXxbAEAA");
+    letmyConversation =new Bard.Chat();
     try {
       
         // Use the askAI function here
-        const response = await askAI("Your question");
+        const response = await myConversation.ask("How are you?")
         res.status(200).json({ name: response })
       
         // Rest of your code
