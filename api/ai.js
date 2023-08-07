da659b050b6f13b395637ec5bf6ab36786173467
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-07 20:59:31 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+const { Chatbot, ChatGPTInput } = require('intellinode');
+
+export default async function handler(req, res) {
+    const input = new ChatGPTInput('You are a helpful assistant.');
+    input.addUserMessage('What is the distance between the Earth and the Moon?');
+    // get the responses from the chatbot
+    const responses = await Chatbot.chat(input);
+    res.status(200).json({ name: responses })
   }
