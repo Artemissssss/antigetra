a14fd9ca7e98380a696ee2ca5e0890cca2447932
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 11:42:25 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -3,12 +3,15 @@ import Bard from "bard-ai";
 export default async function handler(req, res) {
   try {
     // Initialize Bard with your API key
-let myBard =new Bard({	"__Secure-3PSID":"Zgi-mD15rkqONivOepJl7O5mYKfj8BobSyAx41wCrHiGfvmsfCf54HLpug7VtiN-b0VGbg.",});
+let myBard = new Bard({ "__Secure-1PSID": "Zgi-mD15rkqONivOepJl7O5mYKfj8BobSyAx41wCrHiGfvms1lbbdJgwQa3Qnn0g4gdg0g.", "__Secure-1PSIDTS": "sidts-CjEBSAxbGYnNFCtt654peQI9_OAgqrhMXj-Nki6g0vVdGGqlR-O4iyl7Xt8qDAEy4w-rEAA" }, {
+  verbose: true,
+  fetch: fetch,
+});
     // Use the askAI function here
     const response = await myBard.ask("Hello, world!")
     
     // Send the response back as JSON
-    res.status(200).json({ response: response });
+    await res.status(200).json({ response: response });
 
   } catch (error) {
     console.error("Error:", error);
