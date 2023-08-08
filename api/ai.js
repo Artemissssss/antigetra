2447992d3ae5bb393cbb3e53cc2fba905dc16a2d
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 12:00:06 +0200

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,20 +1,22 @@
 import Bard from "bard-ai";
 
 export default async function handler(req, res) {
-  try {
-    // Initialize Bard with your API key
-let myBard = new Bard({ "__Secure-1PSID": "ZgjLfz0-1rUYmumhHGJVVmyIb24AIXNELFz1owxxmHcANlj2PR7ghy7dW-CV3RofeeDu5w.", "__Secure-1PSIDTS": "sidts-CjEBSAxbGQNAWhVcUmqPpNHJlRxhJk5PqP5V4QSxcgGGT-q0TX9TisvQJnDIZzUJM2crEAA" }, {
-  verbose: true,
-  fetch: fetch,
-});
-    // Use the askAI function here
-    const response = await myBard.ask("Hello, world!")
-    
-    // Send the response back as JSON
-    await res.status(200).json({ response: response });
-
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal server error" });
+  if(req.method === "POST"){
+    try {
+      // Initialize Bard with your API key
+  let myBard = new Bard({ "__Secure-1PSID": process.env.SECURE_FIRSR, "__Secure-1PSIDTS": process.env.SECURE_SECOND }, {
+    verbose: true,
+    fetch: fetch,
+  });
+      // Use the askAI function here
+      const response = await myBard.ask(req.body.prompt)
+      
+      // Send the response back as JSON
+      await res.status(200).json({ response: response });
+  
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 }
