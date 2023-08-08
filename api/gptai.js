1cfe90401d152ef384c0ae4090c6b8338a81daf9
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-08 21:08:54 +0200

    Add files via upload

---
+++ api/gptai.js
@@ -0,0 +1,23 @@
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
+
+export default async function handler(req, res) {
+  if(req.method === "POST"){
+    try {
+      // Initialize Bard with your API key
+      const api = new ChatGPTUnofficialProxyAPI({
+        accessToken: process.env.OPENAI_API_KEY
+      })
+    
+      const response = await api.sendMessage(req.body.prompt)
+      
+      // Send the response back as JSON
+      await res.status(200).json({ response: response.text });
+  
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }else{
+    res.status(403).json({message:"Not for this"})
+  }
+}
