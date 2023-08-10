eec872f26f110c547a7323e3e976b4dff68b87a9
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-11 00:36:38 +0200

    Create banwords.js

---
+++ api/banwords.js
@@ -0,0 +1,13 @@
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+    const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('banwords').collection('lgbtqplus');
+    const cursor = coll.find();
+    const result = await cursor.toArray();
+    await client.close();
+    res.status(200).json(result)
+}
