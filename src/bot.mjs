4e7b01f0ab072a702d3873ad3a13409efde0045d
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-10 22:51:29 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -210,7 +210,7 @@ Provide a concise response solely based on the given text and the provided crite
     const data =  { prompt: promptText };
     
     // Змініть URL на ваш фактичний URL API
-    const apiUrl =  "https://this-is-api.run-eu-central1.goorm.site/gpt4-fake";
+    const apiUrl =  "https://antigetra.vercel.app/api/hercai";
     
     // Збільште тайм-аут, якщо це необхідно
     const timeoutMs =  15000; // 15 секунд
