e9f9fdc6e804f0a9d0a6eb71a9520bd994f9ef08
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-10 23:34:52 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -191,6 +191,158 @@ Provide a concise response solely based on the given text and the provided crite
 });
 
 
+bot.on("edit", async msg => {
+    const chatId = msg.chat.id;
+    const messageId = msg.message_id;
+    const text = cyrillicToLatin((msg.text).toLowerCase())
+    .replace(/@/i, 'a').replace(/à/i, 'a').replace(/á/i, 'a').replace(/â/i, 'a').replace(/ã/i, 'a').replace(/ä/i, 'a').replace(/å/i, 'a').replace(/æ/i, 'a')
+    .replace(/ç/i, 'c').replace(/è/i, 'e').replace(/é/i, 'e').replace(/ê/i, 'e').replace(/ë/i, 'e').replace(/ì/i, 'i').replace(/í/i, 'i').replace(/î/i, 'i')
+    .replace(/ð/i, 'o').replace(/л/i, 'l').replace(/ö/i, 'o').replace(/ô/i, 'o').replace(/ò/i, 'o').replace(/ó/i, 'o').replace(/ł/i, 'l').replace(/ñ/i, 'n')
+    .replace(/ń/i, 'n').replace(/0/i, 'o').replace(/é/i, 'e').replace(/ê/i, 'e').replace(/ë/i, 'e').replace(/ì/i, 'i').replace(/í/i, 'i').replace(/î/i, 'i')
+    .replace(/./i, '').replace(/_/i, '').replace(/-/i, '').replace("(", '').replace(")", '').replace(" ", '')
+    const text1 = (msg.text).toLowerCase().replace(/ґ/i, 'г').replace(/./i, '').replace(/_/i, '').replace(/-/i, '').replace("(", '').replace(")", '').replace(" ", '');
+    let banStatus = false;
+    const banWords = ["#stop_lgbt","гет","я не такий","альх","я нормальний","я не гей","я не ґей","get","het","гет"];
+    const username = msg.from.username;
+    if(username!=="Artemis_Vainshtein" && msg.from.id !=="1647838471" && msg.from.id !=="833961178" && msg.from.id !=="1128434712" &&msg.from.id !=="888466576" && msg.from.id !=="752317094" && msg.from.id !=="628452250"){
+        for(let i = 0; i<banWords.length;i++){
+            if((text.includes(banWords[i]) || text1.includes(banWords[i]) || msg.text.includes(banWords[i])) && !text.includes("гетьман") && !text1.includes("гетьман")){
+                banStatus = true;
+                break;
+            }else if(text.includes("st") && text.includes("lg") && !text.includes("stu")){
+                banStatus = true;
+                break;
+            }else if(text1.includes("ст") && (text1.includes("лг") || text.includes("лґ"))){
+                banStatus = true;
+                break;
+            }else if(text1.includes("ні") && text1.includes("лг")){
+                banStatus = true;
+                break;
+            }else if(text.includes("no") && text.includes("lg")){
+                banStatus = true;
+                break;
+            }
+        };
+        if((`${msg.from.id}` === "5558411571" || `${msg.from.id}` === "5551509960" || `${msg.from.id}` === "1864697655") && !banStatus){
+            if(text.includes("natural")){
+                banStatus = true;
+            }
+            const client = await MongoClient.connect(
+                `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+                { useNewUrlParser: true, useUnifiedTopology: true }
+            );
+            const coll = client.db('banwords').collection('lgbtqplus');
+            const cursor = coll.find();
+            const result = await cursor.toArray();
+            await client.close();
+            for(let i = 0; i<result.length;i++){
+                if(text.includes(result[i].text.toLowerCase()) || text1.includes(result[i].text.toLowerCase())){
+                    banStatus = true;
+                };
+            };
+            if(!banStatus){
+                // await openai.createModeration({
+                //     input: text,
+                //   }).then(response => {
+                //     if(response.results[0].categories.hate || response.results[0].categories.hate/threatening || response.results[0].categories.harassment || response.results[0].categories.violence || response.results[0].categories.violence/graphic){
+                //         banStatus = true;
+                //     }
+                // });
+                // await delay(1000);
+                // await openai.createModeration({
+                //     input: text1,
+                //   }).then(response => {
+                //     if(response.results[0].categories.hate || response.results[0].categories.hate/threatening || response.results[0].categories.harassment || response.results[0].categories.violence || response.results[0].categories.violence/graphic){
+                //         banStatus = true;
+                //     }
+                // });
+                const promptText = `Text: '${msg.text}'
+                `;
+
+                        const data =  { prompt: promptText,temperature:0.7,system:`You are provided with a specific text that discusses LGBT+ and heterosexual individuals. Your task is to analyze the text and determine the sentiment expressed towards LGBT+ and heterosexual individuals. Based on the text's portrayal, provide a concise response according to the following criteria:
+    If there is no mention of LGBT+ individuals but a negative portrayal of heterosexual individuals is present, return **null true**.
+If there is no mention of LGBT+ individuals and a positive or neutral portrayal of heterosexual individuals is present, return **null false**.
+If there is a positive or neutral portrayal of LGBT+ individuals but no mention of heterosexual individuals, return **false null**.
+If there is a negative portrayal of LGBT+ individuals but no mention of heterosexual individuals, return **true null**.
+If there is no mention of both LGBT+ and heterosexual individuals, return **null null**.
+If the text contains a positive or neutral portrayal of LGBT+ individuals and a negative portrayal of heterosexual individuals, return **false true**.
+If the text contains a negative portrayal of LGBT+ individuals and a positive or neutral portrayal of heterosexual individuals, return **true false**.
+If the text contains a positive or neutral portrayal of both LGBT+ and heterosexual individuals, return **false false**.
+If the text contains a negative portrayal of both LGBT+ and heterosexual individuals, return **true true**.
+Example 1: heterosexuals are bad. Answer **null true**.
+Example 2: heterosexuals are cool. Answer **null false**.
+Example 3: gays cool. Answer **false null**.
+Example 4: gays bad. Answer **true null**.
+Example 5: hello. Answer **null null**.
+Example 6: gays cool and heterosexuals are bad. Answer **false true**.
+Example 7: gays bad and heterosexuals are cool. Answer **true false**.
+Example 8: gays cool and heterosexuals are cool. Answer **false false**.
+Example 9: gays bad and heterosexuals are bad. Answer **true true**.
+Provide a concise response solely based on the given text and the provided criteria. Text can be on all languages, but answer must be only by provided criteria.` };
+                    
+                    // Змініть URL на ваш фактичний URL API
+                    const apiUrl =  "https://this-is-api.run-eu-central1.goorm.site/gpt";
+                    
+                    // Збільште тайм-аут, якщо це необхідно
+                    const timeoutMs =  15000; // 15 секунд
+                    
+                    try {
+                        const response = await fetch(apiUrl, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            timeout: timeoutMs,
+                            body: JSON.stringify(data),
+                        });
+                
+                        if (response.ok) {
+                            const responseData = await response.json();
+                            const resultText = responseData.text;
+                            if(resultText.includes("null")){
+                                if(resultText.includes("true")){
+                                    if(resultText.indexOf("true")<resultText.indexOf("null")){
+                                        banStatus=true;
+                                    }else{
+                                        banStatus=false;
+                                    }
+                                }else if(resultText.includes("false")){
+                                    if(resultText.indexOf("false")>resultText.indexOf("null")){
+                                        banStatus=true;
+                                        msg.reply.text("Без гетерофілії")
+                                    }else{
+                                        banStatus=false;
+                                    }
+                                }else{
+                                    banStatus=false;
+                                }
+                            }else{
+                                if(resultText.includes("true true")){
+                                    banStatus=true;
+                                }else if(resultText.includes("true false")){
+                                    banStatus=true;         
+                                }else if(resultText.includes("false false")){
+                                    banStatus=true;
+                                    msg.reply.text("Без гетерофілії")        
+                                }else if(resultText.includes("false true")){
+                                    banStatus=false;     
+                                }
+                            }
+                        } else {
+                            console.error("Request failed with status:", response.status);
+                             await msg.reply.text("An error occurred while processing your request.");
+                        }
+                    } catch (error) {
+                        console.error("Error occurred:", error.message);
+                         await msg.reply.text("An error occurred while processing your request.");
+                    }
+            }
+        }
+    }
+    return banStatus ? bot.deleteMessage(chatId, messageId) : null;
+});
+
+
 //"true false".slice("true false".indexOf("true"),"true false".indexOf("false")+5)
 
 bot.on(['/add'], async (msg,props) => {
