ce886f138d6a5619be75fb8163ef0641fa906442
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-10 22:55:41 +0200

    Update hercai.js

--- api/hercai.js
+++ api/hercai.js
@@ -10,7 +10,17 @@ export default async function handler(req, res) {
             // Available Models "v1" | Default Model;"v1"
             client.question({
                 model:"v2",
-                content: req.body.prompt
+                content: `You are provided with a specific text that discusses LGBT+ and heterosexual individuals. Your task is to analyze the text and determine the sentiment expressed towards LGBT+ and heterosexual individuals. Based on the text's portrayal, provide a concise response according to the following criteria:
+                If there is no mention of LGBT+ individuals but a negative portrayal of heterosexual individuals is present, return **null true**.
+            If there is no mention of LGBT+ individuals and a positive or neutral portrayal of heterosexual individuals is present, return **null false**.
+            If there is a positive or neutral portrayal of LGBT+ individuals but no mention of heterosexual individuals, return **false null**.
+            If there is a negative portrayal of LGBT+ individuals but no mention of heterosexual individuals, return **true null**.
+            If there is no mention of both LGBT+ and heterosexual individuals, return **null null**.
+            If the text contains a positive or neutral portrayal of LGBT+ individuals and a negative portrayal of heterosexual individuals, return **false true**.
+            If the text contains a negative portrayal of LGBT+ individuals and a positive or neutral portrayal of heterosexual individuals, return **true false**.
+            If the text contains a positive or neutral portrayal of both LGBT+ and heterosexual individuals, return **false false**.
+            If the text contains a negative portrayal of both LGBT+ and heterosexual individuals, return **true true**.
+            Provide a concise response solely based on the given text and the provided criteria. ${req.body.prompt}`
             }).then(response => {
                 res.status(200).json({
                     response: response.reply
