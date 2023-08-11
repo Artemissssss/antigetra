e01299110eaa1d18900b6b8fa4e43ee5085fdbec
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-11 12:48:23 +0200

    Update gptai.js

--- api/gptai.js
+++ api/gptai.js
@@ -1,5 +1,28 @@
-import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
-import { ChatGPTAuthTokenService } from "chat-gpt-authenticator";
+const { ChatGPTUnofficialProxyAPI } = require('chatgpt');
+
+async function main(requestText) {
+  // Створюємо новий екземпляр ChatGPTUnofficialProxyAPI
+  const chatGPT = new ChatGPTUnofficialProxyAPI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
+
+  // Встановлюємо параметри для ChatGPT
+  chatGPT.setOptions({
+    length: 150,
+    temperature: 0.9,
+    topP: 1,
+    frequencyPenalty: 0,
+    presencePenalty: 0
+  });
+
+  // Надсилаємо повідомлення до ChatGPT
+  const response = await chatGPT.sendMessage(requestText);
+
+  // Виводимо відповідь від ChatGPT
+  return response.text;
+}
+
+
 
 
 export default async function handler(req, res) {
@@ -12,15 +35,15 @@ export default async function handler(req, res) {
       // );
       // const token = await chatGptAuthTokenService.getToken();
 
-      const api = new ChatGPTUnofficialProxyAPI({
-        accessToken: "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",
-        apiReverseProxyUrl: 'https://ai.fakeopen.com/api/conversation'
-      })
+      // const api = new ChatGPTUnofficialProxyAPI({
+      //   accessToken: "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",
+      //   apiReverseProxyUrl: 'https://ai.fakeopen.com/api/conversation'
+      // })
     
-      const response = await api.sendMessage(req.body.prompt)
+      // const response = await api.sendMessage(req.body.prompt)
       
       // Send the response back as JSON
-      await res.status(200).json({ response: response.text });
+      await res.status(200).json({ response: main(req.body.prompt) });
   
     } catch (error) {
       console.error("Error:", error);
