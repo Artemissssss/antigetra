767e062a07b2d001e43a723fc597a8207d0c0e05
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-26 00:31:34 +0200

    Update allmsg.js

--- api/allmsg.js
+++ api/allmsg.js
@@ -5,7 +5,7 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 export default async function handler(req, res) {
     for(let i  = 111000; i<111625;i++){
-        await bot.forwardMessage(1052973544,-1001955166931,i)
+        console.log(await bot.forwardMessage(1052973544,-1001955166931,i))
     }
     res.status(200).json({ error: "Internal server error" });
 }
