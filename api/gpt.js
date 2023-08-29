37819f05b6d3e6a22c7b685cc70400b0deaf9365
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-29 20:20:17 +0200

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -5,21 +5,7 @@ import {
   export default async function handler(req, res) {
     if (req.method === "POST") {
       try {
-                // Initialize Bard with your API key
-        // const chatGptAuthTokenService = new ChatGPTAuthTokenService(
-        //   "astrakiller20@gmail.com",
-        //   "m3MgfG5NVtkKweR"
-        // );
-        // const token = await chatGptAuthTokenService.getToken();
-  
-        // const api = new ChatGPTUnofficialProxyAPI({
-        //   accessToken: "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",
-        //   apiReverseProxyUrl: 'https://ai.fakeopen.com/api/conversation'
-        // })
-  
-        // const response = await api.sendMessage(req.body.prompt)
-  
-        // Send the response back as JSON
+           
         const chatGPT = new ChatGPTUnofficialProxyAPI({
             accessToken: process.env.OPENAI_API_KEY,
             apiReverseProxyUrl: "https://ai.fakeopen.com/api/conversation"
